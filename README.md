5831f41c431d9927bcdad2e32e83748167adbdf4
ko-he- <koheiyatsushiro@yahoo.co.jp>
2016-03-20 20:01:02 +0900

    Add code

--- README.md
+++ README.md
@@ -1,17 +1,33 @@
-virtualenv  venv
-source venv/bin/activate
+
+## 概要
+
+Google Analytics APIをPythonから呼びます。  
+実行すろことで設定した期間内のpageviewによるRankingを 出力します。
+
+## 使い方
+https://developers.google.com/analytics/devguides/config/mgmt/v3/quickstart/service-py?hl=ja#enable
+のステップ1とステップ2までの終了させます。
+
+jsonファイルの準備をします
+
+    cp config.json.sample config.json
+
+config.jsonの中を以下のように編集します。
+
+    {
+        "email": "<your google developer email adress> ex) sample@sample.iam.gserviceaccount.com ",
+        "key": "<*.p12 path> ex) ./sample-5a5a55a5a5a5.p12",
+        "start_date": "ランキングの期間の開始日 ex) 2016-02-07",
+        "end_date": "ランキングの期間の終了日 ex) 2016-03-07"
+    }
+
+以下の二つをpipで入れます
+
+    sudo pip install --upgrade google-api-python-client
+    pip install pyopenssl
 
 
-sudo pip install --upgrade google-api-python-client
-sudo pip install oauth2client
-sudo pip install pycrypto
-sudo pip install pyopenssl
-pip install beautifulsoup4
+実行します
 
+    python googel_analystic_api_ranking.py
 
-
-
-
-
-× SignedJwtAssertionCredentials
-○ ServiceAccountCredentials
--- googel_analystic_api_ranking.py
+++ googel_analystic_api_ranking.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
-
 from apiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
-
+import json
 import httplib2
 
-import json
-import urllib2
-from bs4 import BeautifulSoup
 
 def get_service(api_name, api_version, scope, key_file_location, service_account_email):
     credentials = ServiceAccountCredentials.from_p12_keyfile(
@@ -51,16 +47,11 @@ def get_first_profile_id(service):
     return None
 
 
-def get_summary_results(service, profile_id):
-    return 1
-
-
 def get_rankings_results(service, profile_id, config):
     return service.data().ga().get(
         ids='ga:' + profile_id,
         start_date=config['start_date'],
         end_date=config['end_date'],
-        filters='ga:pagePath!=/',
         sort='-ga:pageviews',
         max_results='10',
         dimensions='ga:pageTitle,ga:pagePath',
@@ -71,43 +62,27 @@ def print_rankings_results(results, home):
     # Print data nicely for the user.
     if results:
         print(u'-----------ランキングTop10-----------')
-        for row in results.get('rows'):
-            html = urllib2.urlopen(u'{0}{1}'.format(home, row[1]).encode('utf-8')).read()
-            print(get_author(html))
-            print(get_posted(html))
-            print(u'{0}\t{1}{2}\t{3}'.format(row[0], home, row[1], row[2]))
+        for col in results.get('rows'):
+            print(u'{0}\t{1}{2}\t{3}'.format(col[0], home, col[1], col[2]))
     else:
         print ('No results found')
-
-
-def get_author(html):
-    soup = BeautifulSoup(html, "html.parser")
-    return soup.find(rel='author').renderContents()
-
-def get_posted(html):
-    soup = BeautifulSoup(html, "html.parser")
-    soup = soup.find(class_='byline')
-    soup.find(class_='vcard author').extract()
-    return soup.renderContents().strip()
 
 
 def main():
     # Define the auth scopes to request.
     scope = ['https://www.googleapis.com/auth/analytics.readonly']
 
-    # Use the developer console and replace the values with your
-    # service account email and relative location of your key file.
+    # 設定ファイルの読み込み
     config_file = open('config.json')
     config = json.load(config_file)
     config_file.close()
     service_account_email = config['email']
     key_file_location = config['key']
 
-    # Authenticate and construct service.
+    # apiの呼び出し・結果出力
     service = get_service('analytics', 'v3', scope, key_file_location, service_account_email)
     profile = get_first_profile_id(service)
     print_rankings_results(get_rankings_results(service, profile, config), config['home'])
-
 
 if __name__ == '__main__':
     main()
